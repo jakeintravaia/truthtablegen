0594b9024bc8c858446f02b36d93f0aa2cfd391a
jakeintravaia <46909741+jakeintravaia@users.noreply.github.com>
2020-11-24 14:57:51 -0500

    Add files via upload

---
+++ example.py
@@ -0,0 +1,7 @@
+import truthtablegen as tt # Import truthtablegen
+
+tt.makeTable(2, 4) # Make a truth table with 2 input variables and 4 outputs
+
+tt.printTable() # Print the table you just created
+
+tt.clearTable() # Clear table values in preparation for new table
